651b8b70887b3ca638a454114212b22ca41c017d
Shiveshdixit07 <shiveshdixit17@gmail.com>
2025-08-16 22:14:58 +0530

    Posted the solution for problem 1323. Maximum 69 Number

---
+++ LeetcodePotdSolutions/1323. Maximum 69 Number.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int maximum69Number(long long num)
+    {
+        string s = to_string(num);
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] != '9')
+            {
+                s[i] = '9';
+                break;
+            }
+        }
+        return stoll(s);
+    }
+};
